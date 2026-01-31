7104d06882231a80c45281a806299ac4a16c1df0
Mounika@1508 <mounikamouni8499@gmail.com>
2026-01-31 21:52:48 +0530

    Added nw files and updated existing ones

--- frontend/backend/app/validations/category-validation.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-const categoryValidationSchema = Joi.object({
-    name: Joi.string().trim().required(),
-    imageUrl: Joi.string()
-})
-
-module.exports = categoryValidationSchema;
--- frontend/backend/app/validations/deliveryBoy-validation.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-const deliveryBoyValidationSchema = Joi.object({
-    phone: Joi.string().trim().required().min(10).max(15),
-    address: Joi.string().trim().required().min(3).max(256),        
-    city: Joi.string().trim().required().min(2).max(64),
-    vehicleNumber: Joi.string().trim().required().min(3).max(20)
-});  
-module.exports = deliveryBoyValidationSchema;   
